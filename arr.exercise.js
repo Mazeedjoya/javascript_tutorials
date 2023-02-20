606a02c60afa2aa6d8df0e7ff3aa79ce13de844d
Mazeedjoya <105007657+Mazeedjoya@users.noreply.github.com>
2023-02-20 19:28:47 +0530

    Add files via upload

---
+++ arr.exercise.js
@@ -0,0 +1,141 @@
+// todo 20 feb 2023
+
+  //! 1 => find MAX no. from array
+  //  let arr = [3,4,7,6,8,2 ,1];
+  // document.write("Max Value in Array =>" ,Math.max(...arr ))
+
+
+        //! 2 => find MIN no. from array
+        //  let arr = [3,4,7,6,8,2,1];
+        // document.write("Min Value in Array =>", Math.min(...arr ))
+
+        // ! 3  Find total duplicate count of a number 
+                    // let arr = [1,2,3,3,4,3,2,1];
+                    // let count ={};
+            
+                    // for(var i=0; i<arr.length; i++){
+
+                    // count[arr[i]] = (count[arr[i]] || 0) + 1 ;
+                    //     }; 
+
+                    //     console.log(count);
+        
+
+
+
+        // ! 4 find middle element of array
+        //         let arr = [1,2,3,9,5,6,7];
+        //         let arrlen = arr.length;
+        //         console.log(arrlen/2);
+        //         let ods = Math.floor(arrlen/2)
+        //         if(arrlen%2==0)
+        //         {
+        //             document.write(` Array length is Even ${arr[arrlen/2-1]} & ${ arr[arrlen/2]} is middle element`)
+        //         }else if(arrlen%2!=0) {
+        //             console.log(`Array lenght is Odd ${arr[ods]}`)
+                // }
+
+
+        // ! 5 find last element of array
+
+        // let arr = [20,30,40,50,60];
+        // let lastElement = arr.length-1;
+        // document.write(arr[lastElement])
+
+        // ! 6 find first element of array
+
+        // let arr = [10,20,30,40,50];
+        // let firstElement = arr[0];
+        // document.write(firstElement)
+
+
+
+        //! 8 find total count of odd no. in array
+        // todo 
+        // let arr = [2,3,4,5,6,7,8,9];
+        // let arr1 =[];
+
+        // for(var i=0 ; i<arr.length; i++){
+        //     if(arr[i]%2!=0){
+        //        arr1.push(arr[i])
+        //     }
+        // }
+        // document.write("Total count of odd no is =>  ",arr1.length)
+
+
+            
+        // ! 9 find total count of even no. in array
+        // todo 
+
+        // let arr = [ 1,2,3,4,5,6,7,8,9, 10];
+        // let arr1 =[];
+        // for(var i=0; i<arr.length; i++){
+        //     if(arr[i]%2==0){
+        //         arr1.push(arr[i])
+        //     }
+        // }
+        // document.write("Total count of EVEN no is => ", arr1.length)
+
+
+        // ! 10 find second highest no. from array
+
+        // let arrlen = Number(prompt("Enter array length"));
+        // let arr =[]
+        // for(var i=0; i<arrlen; i++){
+        // let value = prompt("Enter value");
+        //  arr.push(value);
+        
+        // }
+        // arr.sort();
+        // console.log(arr);
+        // console.log(arr[arrlen-2]);
+
+        // ! 11. find second lowest number from array 
+        // let arrlen = Number(prompt("Enter array length"));
+        // let arr =[]
+        // for(var i=0; i<arrlen; i++){
+        // let value = prompt("Enter value");
+        //  arr.push(value);
+        
+        // }
+        // console.log(arr);
+        // arr.sort();
+
+        // console.log(arr[1]);
+
+
+        // ! 12. Find average of the array
+
+        // let arr = [10,20,30,40,50]
+        // let sum=0;
+        // let avg ;
+        // for(var i=0; i<arr.length; i++){
+        //     sum+=arr[i];
+        //      avg = sum/arr.length;
+        // }
+        // console.log(avg);
+
+
+// !  13 find which number is repeated most number of the times 
+//! in the array, also tell the number along with count
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
